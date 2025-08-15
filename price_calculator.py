95768d952648492391fed8cb68edfb2fe3d6e033
eduxcode <daviedu800@gmail.com>
2025-08-15 13:57:20 -0300

    Finished

---
+++ price_calculator.py
@@ -0,0 +1,10 @@
+product_name = "Cadeira Infantil"
+unit_price = 12.40
+quantity = 3
+
+total_price = unit_price * quantity
+
+print(f"Produto: {product_name}")
+print(f"Preço Unitário: R$ {unit_price:.2f}")
+print(f"Quantidade: {quantity}")
+print(f"Preço Total: R$ {total_price:.2f}")
---
+++ volume_calculator.py
@@ -0,0 +1,7 @@
+length = 12
+width = 14
+height = 20
+
+volume = length * width * height
+
+print(f"O volume da caixa retangular é: {volume} cm³")
